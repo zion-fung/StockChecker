3f9969d9925285f1d775421e1a32bd1ca7604b0d
RPI 4 <zfung01@gmail.com>
2020-06-12 14:06:27 -0700

    Removed local path for file arg for rpi4

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ if (argv.f && argv.file) {
     let filepath = argv.f ? argv.f : argv.file;
     let emailText = null;
     if (filepath) {
-        const storeList = require(`./${filepath}`);
+        const storeList = require(`${filepath}`);
         emailText = await stores.main(storeList);
     } else {
         emailText = await stores.main([]);
