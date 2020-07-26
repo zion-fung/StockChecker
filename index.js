e5f3761a79a5bd4f3943325b00b41865a94044d3
Zion Fung <zionfung1@gmail.com>
2020-07-25 21:48:32 -0700

    Added logging

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const texter = require("./texter");
 
 const stores = require("./stores/");
 
-const argvDefaults = { nospam: false };
+const logger = require("./logger");
+
+const argvDefaults = { nospam: false, executablePath: null };
 
 const argv = require('minimist')(process.argv.slice(2), { default: argvDefaults });
 
@@ -21,22 +23,30 @@ if (argv.m && argv.method) {
 (async () => {
     // If the nospam flag is set, don't send an email/text if nothing is in stock
     const nospam = argv.nospam;
+
     const filepath = argv.f ? argv.f : argv.file;
-    const method = argv.m ? argv.m : argv.method;
     const storeList = filepath ? require(filepath) : [];
+
+    const method = argv.m ? argv.m : argv.method;
+    
+    const executablePath = argv.executablePath; // For RPI 4
 
     // Get html results for email, plaintext for texting
     if (!method || method === "email") {
         const message = await stores.main(storeList, true, nospam);
         if (message.length === 0) { // If nospam is sent and the message is empty, don't send anything
+            logger.log(`Skipped ${storeList} (email)`);
             return;
         }
+        logger.log(`Emailing ${storeList}`);
         await emailer.sendEmail(message);
     } else {
         const message = await stores.main(storeList, false, nospam);
         if (message.length === 0) { // If nospam is sent and the message is empty, don't send anything
+            logger.log(`Skipped ${storeList} (text)`);
             return;
         }
+        logger.log(`Texting ${storeList}`);
         await texter.sendMessage(message);
     }
 })();
